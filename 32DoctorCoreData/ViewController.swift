dc85c6433d76ebc87838c69341a70d56ea2f882c
mbeascoa <miguel.beascoabeastek.net>
2021-09-10 10:25:52 +0200

    Initial Commit

---
+++ 32DoctorCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  32DoctorCoreData
+//
+//  Created by mbeascoa on 10/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
